8ad690fab65efc8c81c3198b4e5d2592f3e712a4
William Post <wpost@hawaii.edu>
2020-09-16 20:39:34 -1000

    added using static middleware for serving frontend stuffs

--- app/server.ts
+++ app/server.ts
@@ -1,4 +1,6 @@
-import {Express, Request, Response} from "express";
+import {Express, Request, Response, static as staticMiddleware} from "express";
+
+const CLIENT_BUILD_PATH = './build/frontend';
 
 export class Server {
 
@@ -7,9 +9,15 @@ export class Server {
     constructor(app: Express) {
         this.app = app;
 
+        app.use(staticMiddleware(CLIENT_BUILD_PATH));
+
         this.app.get("/api", (req: Request, res: Response): void => {
             res.send("You have reached the API!");
-        })
+        });
+
+        this.app.get("*", (req: Request, res: Response): void => {
+            res.sendFile(CLIENT_BUILD_PATH + '/index.html');
+        });
     }
 
     public start(port: number): void {
